0e1fdf3e6ea26783cc27cccc11a1195d60faa310
sebnunez6 <snunez6@miners.utep.edu>
2018-09-02 21:56:24 -0600

    dictionary with word repititions

--- wordCount.py
+++ wordCount.py
@@ -3,7 +3,8 @@
 import sys        # command line arguments
 import os         # checking if file exists
 
-
+print("test")
+print("test2")
 # set input and output files
 if len(sys.argv) is not 3:
     print("Correct usage: wordCountTest.py <input text file> <output file>")
@@ -16,12 +17,21 @@ if not os.path.exists(textFile):
     print ("text file input %s doesn't exist! Exiting" % textFile)
     exit()
 
+if not os.path.exists(outputFile):
+    print ("outfile input %s doesn't exist! Exiting" % textFile)
+    print ("creating textFile")
+
+
 wordTracker = {}
 fo = open(textFile, "r")
 str = fo.read()
 list = str.split()
 for word in list:
-	resultingword = word.lower().replace(".","").replace(",","").replace(";","").replace(":","")
-	if(wordTracker.has_key(resultingword))
-		
+	resultingword = word.lower().replace(".","").replace("\"","").replace("'","").replace(",","").replace(";","").replace(":","")
+	#print(resultingword)
+	if wordTracker.has_key(resultingword):
+		wordTracker[resultingword]+=1
+	else:
+		wordTracker.setdefault(resultingword,1)
+wordTracker = sorted(wordTracker)
 
