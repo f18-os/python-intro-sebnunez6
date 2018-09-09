8ea702a2e4a48d3dd1c0f7aedb9f557d13211154
sebnunez6 <snunez6@miners.utep.edu>
2018-09-08 20:14:34 -0600

    Readme added and shell.py cleaned.

---
+++ shell/Readme.md
@@ -0,0 +1,3 @@
+##Shell
+Once you run the program it ask for the user input. From the user input it will
+execute the command and output it onto outputShell.txt
--- shell/shell.py
+++ shell/shell.py
@@ -13,26 +13,24 @@ if rc < 0:
     sys.exit(1)
 
 elif rc == 0:                   # child
-   # os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
-   #              (os.getpid(), pid)).encode())
     os.write(1, ("Please enter input\n".encode()))
     args = input().split()
 
-    os.close(1)                 # redirect child's stdout
-    sys.stdout = open("output.txt", "w")
-    fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
+    os.close(1)                
+    sys.stdout = open("outputShell.txt", "w")  # redirect child's stdout
+    fd = sys.stdout.fileno() 
     os.set_inheritable(fd, True)
 
-    for dir in re.split(":", os.environ['PATH']): # try each directory in path
+    for dir in re.split(":", os.environ['PATH']):  # try each directory in path
         program = "%s/%s" % (dir, args[0])
         try:
-            os.execve(program, args, os.environ) # try to exec program
-        except FileNotFoundError:             # ...expected
-            pass                              # ...fail quietly 
+            os.execve(program, args, os.environ)  # try to exec program
+        except FileNotFoundError:                 # ...expected
+            pass                                  # ...fail quietly 
 
     os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
     sys.exit(1)                 # terminate with error
 
-else:                           # parent (forked ok)
+else:                           # parent fork 
     childPidCode = os.wait()
     os.write(1, ("Command executed\n".encode()))
